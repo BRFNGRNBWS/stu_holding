acf005f6dafcc80b26c503a5ede155b0da1832d0
colin <clnlws44@gmail.com>
2020-07-26 20:04:37 -0700

    functional?

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.scss';
 import SearchBar from './SearchBar';
 import $ from 'jquery';
 import ImageMapper from 'react-image-mapper';
-			
-const coordPercentages = [[0.28981, 0.34518, 0.75370, 0.35926, 0.72500, 0.70741, 0.28611, 0.70519]];
+import Perspective from './perspective.js'
+
+//coordinates of the corners of each cutout, expressed as a percentage of the image width/height
+// format [x, y, x, y, x, y...]
+//always start coords from the top left of where the album should be and go clockwise
+const coordPercentages = [[0.28981, 0.34518, 0.75370, 0.35926, 0.72500, 0.70741, 0.28611, 0.70519], [0.74537, 0.32370, 0.83981, 0.32148, 0.84444, 0.39407, 0.74907, 0.39556]];
 const imgSize = {x: 486, y: 608};
 var scale = 1.0;
 var maps = {name: "my-map", areas: []};
+var placePicked = 0;
 genMaps();
 
 function updateMapSize(w){
-	this.setState({width: w});
+	this.setState({canWidth: w});
 };
 
 function resizeStage(w){
@@ -35,28 +40,64 @@ function calcCoords(which){
 
 function genMaps(){
 	for (var i = 0; i < coordPercentages.length; i++){
-		maps["areas"].push({name: i.toString(), shape: "poly", coords: calcCoords(i), strokeColor: "red"});
+		maps["areas"].push({name: i.toString(), shape: "poly", coords: calcCoords(i), preFillColor: "rgba(0, 0, 0, 0.1)"});
 	};
 };
 
+function spotClicked(area){
+	var number = parseInt(area["name"]);
+	placePicked = number;
+	for (var i = 0; i < maps["areas"].length; i++){
+		if (i === placePicked)
+			maps["areas"][i]["preFillColor"] = "rgba(255, 0, 0, 0.2)";
+		else
+			maps["areas"][i]["preFillColor"] = "rgba(0, 0, 0, 0.1)";
+	};
+	this.setState({map: maps});
+};
+
+function renderAlbum(album){
+	console.log(album);
+	var canvas = document.getElementById("img" + placePicked);
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	var img = new Image();
+	img.onload = function(){
+		var p = new Perspective(ctx, img);
+		var m = maps["areas"][placePicked]["coords"];
+		p.draw([
+			[m[0], m[1]],
+			[m[2], m[3]],
+			[m[4], m[5]],
+			[m[6], m[7]]
+		]);
+	};
+	img.src = album["image"][album["image"].length - 1]["#text"];
+};
+
 $(document).ready(function(){
+	spotClicked({name: "0"});
 	resizeStage($(".container").width());
+	$(window).resize(function(){
+		resizeStage($(".container").width());
+	});
 });
 
-$(window).resize(function(){
-	resizeStage($(".container").width());
-});
+
 
 class App extends React.Component {
 	constructor(props){
 		super(props);
-		this.state = {albumPicked: {}, placePicked: 0};
+		this.state = {canWidth: imgSize.x};
+	};
+	
+	componentDidMount(){
+		updateMapSize = updateMapSize.bind(this);
 	};
 	
 	//updated when an album is picked from the search bar
 	updateAlbum = (album) => {
-		this.setState({albumPicked: album})
-		
+		renderAlbum(album);
 	};
 	
 	render(){
@@ -74,8 +115,9 @@ class App extends React.Component {
 							<br />
 							
 							<div className="pictureHolder">
-								<canvas className="albumStage" id="imgStage" width={imgSize.x} height={imgSize.y} />
-								<ImgMap className="imgWithMap" src="./stu_small.png" imgWidth={imgSize.x} width={imgSize.x} map={maps} onClick={area => this.clicked(area)} />
+								<canvas className="albumStage" id="img0" width={imgSize.x} height={imgSize.y} />
+								<canvas className="albumStage" id="img1" width={imgSize.x} height={imgSize.y} />
+								<ImgMap className="imgWithMap" src="/stu_small.png" imgWidth={imgSize.x} width={this.state.canWidth} map={maps} onClick={area => spotClicked(area)} />
 							</div>
 							
 						</main>
@@ -86,12 +128,14 @@ class App extends React.Component {
 	};
 }
 
-class ImgMap extends ImageMapper {
+class ImgMap extends ImageMapper{
 	constructor(props){
-		super(props);
-		console.log(props);
-		updateMapSize = updateMapSize.bind(this);
+		super(props)
 	};
-};	
+	
+	componentDidMount(){
+		spotClicked = spotClicked.bind(this);
+	};
+};
 			
 export default App;
